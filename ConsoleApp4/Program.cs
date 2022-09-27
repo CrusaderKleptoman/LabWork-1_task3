4939ec35c9e2969708742b76ce4c66e77822f90a
CrusaderKleptoman <64123771+CrusaderKleptoman@users.noreply.github.com>
2022-09-28 00:58:10 +0800

    Initial commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab1_3
+{
+    public static class Logic
+    {
+        public static int GetSum(string rowSum)
+        {
+
+            string[] rowSumMembers = rowSum.Split('+', '-');
+
+            char[] separators = new char[] { ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string[] memberSign = rowSum.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            int sum = int.Parse(rowSumMembers[0]);
+
+            for (int i = 1; i < rowSumMembers.Length; i++)
+            {
+                if (memberSign[i - 1].Equals("-"))
+                {
+                    sum -= int.Parse(rowSumMembers[i]);
+                    continue;
+                }
+
+                sum += int.Parse(rowSumMembers[i]);
+            }
+
+            return sum;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+
+            string rowSum = Console.ReadLine();
+
+            int sum = Logic.GetSum(rowSum);
+            Console.WriteLine("Сумма в тексте = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp4Tests/LogicTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab1_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_3.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetSumTest1()
+        {
+            var text = "111+10-30";
+            var sum = 91;
+
+            var message = Logic.GetSum(text);
+
+            Assert.AreEqual(sum, message);
+        }
+
+        [TestMethod()]
+        public void GetSumTest2()
+        {
+            var text = "111";
+            var sum = 111;
+
+            var message = Logic.GetSum(text);
+
+            Assert.AreEqual(sum, message);
+        }
+
+        [TestMethod()]
+        public void GetSumTest3()
+        {
+            var text = "1+10-45-10+4";
+            var sum = -40;
+
+            var message = Logic.GetSum(text);
+
+            Assert.AreEqual(sum, message);
+        }
+    }
+}
